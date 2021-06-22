a5dc1800cadf11b04535c62e9040adcefced2ed7
Komro <34216@nelnet.net>
2021-06-22 16:10:52 -0500

    NumberManipulator Exercise

---
+++ NumberManipulator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace NumberManipulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            string response = Console.ReadLine();
+            double numResponse = Int32.Parse(response);
+            numResponse = (Math.Pow(numResponse,2) + 14 -3);
+            Console.WriteLine(numResponse);
+        }
+    }
+}
